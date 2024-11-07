f0c4ca1bf986eb4ba03e29b44c56379248e38092
Vanessa Peixoto <vanessa@mbp-de-vanessa.home>
2024-11-07 15:10:09 +0100

    first commit

---
+++ src/component/Modal.tsx
@@ -0,0 +1,10 @@
+function Modal() {
+
+  return (
+    <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default Modal;
